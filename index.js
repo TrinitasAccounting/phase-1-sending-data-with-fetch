ff4be60d3ee2e8ae5b4967a7d7f588ed6f52d1ad
Clay Mangum <clay@trinitasaccounting.com>
2024-02-03 05:55:41 -0600

    finally completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+
+
+
+
+const submitData = (name, email) => {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+        .then(res => res.json())
+        .then(data => document.body.append(data.id))
+        .catch((error) => {
+            alert('something went wrong');
+            document.body.append(error.message);
+        })
+};
+
